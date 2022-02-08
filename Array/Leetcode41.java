777be4584edd666ac63dedc8563ffa2f904052ee
Chandra Shekhar Agrawal <csa.jpr08@gmail.com>
2022-02-08 23:51:05 +0530

    Create Leetcode41.java

---
+++ Array/Leetcode41.java
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/first-missing-positive/
+
+// My solution (Java) using cyclic sort
+
+class Solution {
+    public int firstMissingPositive(int[] arr) {
+        
+        //edge case if arr contain all continuous positives after sorting like {1,2,3} ans = 4
+              
+        CyclicSort(arr);
+        //System.out.println(Arrays.toString(arr));
+        int ans =0;
+        boolean flag = true;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]!= i+1){
+                ans = i+1;
+                flag = false;
+                break;
+            }
+        }
+        //System.out.println(ans);
+        if (flag)
+            return arr[arr.length-1]+1;
+        return ans;
+    }
+    
+    public void CyclicSort(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+            int correctindex = arr[i]-1;
+
+            if(arr[i]>0 && correctindex< arr.length &&  arr[i]!= arr[correctindex]){
+                swap(arr, i, correctindex);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+
+    public void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
