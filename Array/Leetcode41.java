d8182216e2b47b82af43d8ac5f5f455134497e74
Chandra Shekhar Agrawal <csa.jpr08@gmail.com>
2024-03-28 21:53:00 +0530

    Updated Leetcode 41 using in place modifying array values
    This approach is not intuitive from interveiw POV

--- Array/Leetcode41.java
+++ Array/Leetcode41.java
@@ -1,45 +1,27 @@
-// https://leetcode.com/problems/first-missing-positive/
+// Porblem Statement - Leetcode 41 - https://leetcode.com/problems/first-missing-positive/
 
-// My solution (Java) using cyclic sort
+// My solution
 
 class Solution {
-    public int firstMissingPositive(int[] arr) {      
-        CyclicSort(arr);
-        //System.out.println(Arrays.toString(arr));
-        int ans =0;
-        boolean flag = true;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]!= i+1){
-                ans = i+1;
-                flag = false;
-                break;
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(nums[i] <= 0 || nums[i] > n){
+                nums[i] = n+1;
             }
         }
-        //System.out.println(ans);
-        //edge case if arr contain all continuous positives after sorting like {1,2,3} ans = 4
-        
-        if (flag)
-            return arr[arr.length-1]+1;
-        return ans;
-    }
-    
-    public void CyclicSort(int[] arr){
-        int i = 0;
-        while(i < arr.length){
-            int correctindex = arr[i]-1;
-
-            if(arr[i]>0 && correctindex< arr.length &&  arr[i]!= arr[correctindex]){
-                swap(arr, i, correctindex);
+        for(int i=0; i<n; i++){
+            int value = Math.abs(nums[i]);
+            if(value <= n && nums[value - 1] > 0){
+                nums[value - 1] = 0 - nums[value - 1];
             }
-            else{
-                i++;
+            
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i] > 0){
+                return i+1;
             }
         }
-    }
-
-    public void swap(int[] arr, int a, int b){
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
+        return n+1;
     }
 }
