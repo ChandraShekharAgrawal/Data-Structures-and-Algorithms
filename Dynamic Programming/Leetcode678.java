5adf64bdc0bee31ddeead53572f0b24a62f5912c
Chandra Shekhar Agrawal <csa.jpr08@gmail.com>
2024-05-09 13:09:23 +0530

    Update Leetcode 678 approach using 2 stacks in O(n)

--- Dynamic Programming/Leetcode678.java
+++ Dynamic Programming/Leetcode678.java
@@ -5,45 +5,38 @@
 class Solution {
     public boolean checkValidString(String s) {
         int n = s.length();
-        int[][] dp = new int[n][n+1];
+        Stack<Integer> openBracket = new Stack<>();
+        Stack<Integer> special = new Stack<>();
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                dp[i][j] = -1;
+            char ch = s.charAt(i);
+            if(ch == '('){
+                openBracket.push(i);
+            }
+            else if(ch == '*'){
+                special.push(i);
+            }
+            else{
+                if(openBracket.size() == 0 && special.size() == 0){
+                    return false;
+                }
+                else if(openBracket.size() != 0){
+                    openBracket.pop();
+                }
+                else {
+                    special.pop();
+                }
             }
         }
-        checkParenthesis(s, dp, 0, 0, n);
-        if(dp[0][0] == 0){
-            return false;
-        }
-        return true;
-    }
-
-    int checkParenthesis(String s, int[][] dp, int index, int openBracket, int n){
-        if(index == n){
-            if(openBracket == 0)
-                return 1;
-            return 0;
-        }
-        if(dp[index][openBracket] != -1){
-            return dp[index][openBracket];
-        }
-        dp[index][openBracket] = 0;
-        if(s.charAt(index) == '('){
-            dp[index][openBracket] = checkParenthesis(s, dp, index+1, openBracket+1, n);
-        }
-        else if(s.charAt(index) == ')'){
-            if(openBracket > 0){
-                dp[index][openBracket] = checkParenthesis(s, dp, index+1, openBracket-1, n);
+        while(openBracket.size() != 0 && special.size() != 0){
+            if(openBracket.peek() > special.peek()){
+                return false;
             }
+            openBracket.pop();
+            special.pop();
         }
-        else{
-            
-            dp[index][openBracket] = dp[index][openBracket] | checkParenthesis(s, dp, index+1, openBracket+1, n);
-            if(openBracket > 0){
-                dp[index][openBracket] = dp[index][openBracket] | checkParenthesis(s, dp, index+1, openBracket-1, n);
-            }
-            dp[index][openBracket] = dp[index][openBracket] | checkParenthesis(s, dp, index+1, openBracket, n);           
+        if(openBracket.size() == 0){
+            return true;
         }
-        return dp[index][openBracket];
+        return false;
     }
 }
